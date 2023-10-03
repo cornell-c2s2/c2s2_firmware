1776fcb7f37adc17c2ea6b84a1fa3a1d58201ca9
Aidan <acm289@cornell.edu>
2023-10-03 17:38:18 -0400

    Reverted SPI port

--- project-group99/sim/SPI_v3/components/SPITestHarness.py
+++ project-group99/sim/SPI_v3/components/SPITestHarness.py
@@ -46,7 +46,9 @@ class SPITestHarness( object ):
     # s.dut.apply( Mamba2020( print_line_trace=trace ) )
 
     if s.is_phy_test:
-      port = '/dev/tty.usbserial-DO01JGPK'
+      # Change port to your necessary port
+      port = '/dev/ttyUSB0'
+      # port = '/dev/tty.usbserial-DO01JGPK'
       # keep retrying since SPI Driver sometimes doesnt work
       while True:
         try:
